7d7920604b777122e7fd7eabaad4c35cbe5d938a
gooroom <gooroom@gooroom.kr>
2016-03-07 16:22:44 +0900

    gooroomupdate translation added

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 
 msgfmt po/xfce4-whiskermenu-plugin/xfce4-whiskermenu-plugin-ko.po \
 		-o usr/share/locale/ko/LC_MESSAGES/xfce4-whiskermenu-plugin.mo 
+
+msgfmt po/gooroomupdate/gooroomupdate-ko.po \
+		-o usr/share/gooroom/locale/ko/LC_MESSAGES/gooroomupdate.mo
