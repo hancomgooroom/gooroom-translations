16772afcbb373bfe31951425b596a209dae09433
gooroom <gooroom@gooroom.kr>
2016-03-08 10:37:36 +0900

    rename gooroomupdate to gooroom-update

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 msgfmt po/xfce4-whiskermenu-plugin/xfce4-whiskermenu-plugin-ko.po \
 		-o usr/share/locale/ko/LC_MESSAGES/xfce4-whiskermenu-plugin.mo 
 
-msgfmt po/gooroomupdate/gooroomupdate-ko.po \
-		-o usr/share/gooroom/locale/ko/LC_MESSAGES/gooroomupdate.mo
+msgfmt po/gooroom-update/gooroom-update-ko.po \
+		-o usr/share/gooroom/locale/ko/LC_MESSAGES/gooroom-update.mo
