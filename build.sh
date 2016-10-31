c556408a143d946ceb3ba9b1b841a0641ac1919d
jongkyung.woo <jkwoo@gooroom.kr>
2016-10-31 22:49:24 +0900

    add live-installer-ko.po
    Change-Id: I8dad3451762a9a4271a196cc214cefebae948211

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-msgfmt po/xfce4-whiskermenu-plugin/xfce4-whiskermenu-plugin-ko.po \
+msgfmt -v -c po/xfce4-whiskermenu-plugin/xfce4-whiskermenu-plugin-ko.po \
 		-o usr/share/locale/ko/LC_MESSAGES/xfce4-whiskermenu-plugin.mo 
 
-msgfmt po/gooroom-update/gooroom-update-ko.po \
+msgfmt -v -c po/gooroom-update/gooroom-update-ko.po \
 		-o usr/share/gooroom/locale/ko/LC_MESSAGES/gooroom-update.mo
 
-msgfmt po/gooroom-sources/gooroom-sources-ko.po \
+msgfmt -v -c po/gooroom-sources/gooroom-sources-ko.po \
 		-o usr/share/gooroom/locale/ko/LC_MESSAGES/gooroom-sources.mo
+
+msgfmt -v -c po/live-installer/live-installer-ko.po \
+		-o usr/share/gooroom/locale/ko/LC_MESSAGES/live-installer.mo
