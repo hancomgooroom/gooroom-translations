071ba4a8aec939fcc23fe5e8295b44333afa8fc2
jongkyung.woo <jkwoo@gooroom.kr>
2018-01-24 15:05:12 +0900

    Added glib20-ko.po
    - Updated "Error splicing file" string
    - Changed gooroom translations directory to /usr/share/gooroom/locale/ko/LC_MESSAGES
    
    Change-Id: Iefe87e4805cf07e624c85762b32d9dc194ac8324

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-msgfmt -v -c po/xfce4-whiskermenu-plugin/xfce4-whiskermenu-plugin-ko.po \
-		-o usr/share/locale/ko/LC_MESSAGES/xfce4-whiskermenu-plugin.mo 
-
 msgfmt -v -c po/gooroom-update/gooroom-update-ko.po \
 		-o usr/share/gooroom/locale/ko/LC_MESSAGES/gooroom-update.mo
 
@@ -13,10 +10,13 @@ msgfmt -v -c po/live-installer/live-installer-ko.po \
 		-o usr/share/gooroom/locale/ko/LC_MESSAGES/live-installer.mo
 
 msgfmt -v -c po/galculator/galculator-ko.po \
-		-o usr/share/locale/ko/LC_MESSAGES/galculator.mo
+		-o usr/share/gooroom/locale/ko/LC_MESSAGES/galculator.mo
 
 msgfmt -v -c po/grub/grub-ko.po \
 		-o usr/share/locale/ko/LC_MESSAGES/grub.mo
 
 msgfmt -v -c po/grub/grub-ko.po \
 		-o boot/grub/locale/ko.mo
+
+msgfmt -v -c po/glib2.0/glib20-ko.po \
+		-o usr/share/gooroom/locale/ko/LC_MESSAGES/glib20.mo
