f0be5fda63ada504f27ef382cb5b4835431cae4d
jongkyung.woo <jkwoo@gooroom.kr>
2019-06-27 10:50:15 +0900

    Updated gooroom-2.0 environments
    Change-Id: I65de69549c32359847d71947ef41921b87f82f21

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Gooroom
+mkdir -p usr/share/gooroom/locale/ko/LC_MESSAGES
+
 msgfmt -v -c po/gooroom-update/gooroom-update-ko.po \
 		-o usr/share/gooroom/locale/ko/LC_MESSAGES/gooroom-update.mo
 
@@ -9,14 +12,20 @@ msgfmt -v -c po/gooroom-sources/gooroom-sources-ko.po \
 msgfmt -v -c po/live-installer/live-installer-ko.po \
 		-o usr/share/gooroom/locale/ko/LC_MESSAGES/live-installer.mo
 
+# Debian
+mkdir -p usr/share/gooroom/locale/debian/galculator/ko
 msgfmt -v -c po/galculator/galculator-ko.po \
-		-o usr/share/gooroom/locale/ko/LC_MESSAGES/galculator.mo
+		-o usr/share/gooroom/locale/debian/galculator/ko/galculator.mo
 
+mkdir -p usr/share/gooroom/locale/debian/glib2.0/ko
+msgfmt -v -c po/glib2.0/glib20-ko.po \
+		-o usr/share/gooroom/locale/debian/glib2.0/ko/glib20.mo
+
+# Grub
+mkdir -p usr/share/gooroom/locale/debian/grub/ko
 msgfmt -v -c po/grub/grub-ko.po \
-		-o usr/share/locale/ko/LC_MESSAGES/grub.mo
+		-o usr/share/gooroom/locale/debian/grub/ko/grub.mo
 
+mkdir -p boot/grub/locale
 msgfmt -v -c po/grub/grub-ko.po \
 		-o boot/grub/locale/ko.mo
-
-msgfmt -v -c po/glib2.0/glib20-ko.po \
-		-o usr/share/gooroom/locale/ko/LC_MESSAGES/glib20.mo
