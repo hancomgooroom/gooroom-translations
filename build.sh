b60d85e2f9a4ff7c7a921e7724376f3579a35448
jongkyung.woo <jkwoo@gooroom.kr>
2019-06-28 16:35:58 +0900

    Added gnome-flashback po and gnome-mpv
    Change-Id: I015db5ce587004cb129e9f022f9ec1e30ac234ae

--- build.sh
+++ build.sh
@@ -1,31 +1,37 @@
-#!/bin/sh
+#!/bin/bash
+
+set -ex
+
+# Gooroom PO
+gooroom_po=(
+gooroom-update
+gooroom-sources
+live-installer
+)
+
+# Debian PO
+debian_po=(
+gnome-flashback
+gnome-mpv
+galculator
+glib20
+grub
+)
 
 # Gooroom
 mkdir -p usr/share/gooroom/locale/ko/LC_MESSAGES
-
-msgfmt -v -c po/gooroom-update/gooroom-update-ko.po \
-		-o usr/share/gooroom/locale/ko/LC_MESSAGES/gooroom-update.mo
-
-msgfmt -v -c po/gooroom-sources/gooroom-sources-ko.po \
-		-o usr/share/gooroom/locale/ko/LC_MESSAGES/gooroom-sources.mo
-
-msgfmt -v -c po/live-installer/live-installer-ko.po \
-		-o usr/share/gooroom/locale/ko/LC_MESSAGES/live-installer.mo
+for po_name in "${gooroom_po[@]}"; do
+    msgfmt -v -c po/$po_name/$po_name-ko.po \
+              -o usr/share/gooroom/locale/ko/LC_MESSAGES/$po_name.mo
+done
 
 # Debian
-mkdir -p usr/share/gooroom/locale/debian/galculator/ko
-msgfmt -v -c po/galculator/galculator-ko.po \
-		-o usr/share/gooroom/locale/debian/galculator/ko/galculator.mo
-
-mkdir -p usr/share/gooroom/locale/debian/glib2.0/ko
-msgfmt -v -c po/glib2.0/glib20-ko.po \
-		-o usr/share/gooroom/locale/debian/glib2.0/ko/glib20.mo
+for po_name in "${debian_po[@]}"; do
+    mkdir -p usr/share/gooroom/locale/debian/$po_name/ko
+    msgfmt -v -c po/$po_name/$po_name-ko.po \
+              -o usr/share/gooroom/locale/debian/$po_name/ko/$po_name.mo
+done
 
 # Grub
-mkdir -p usr/share/gooroom/locale/debian/grub/ko
-msgfmt -v -c po/grub/grub-ko.po \
-		-o usr/share/gooroom/locale/debian/grub/ko/grub.mo
-
 mkdir -p boot/grub/locale
-msgfmt -v -c po/grub/grub-ko.po \
-		-o boot/grub/locale/ko.mo
+msgfmt -v -c po/grub/grub-ko.po -o boot/grub/locale/ko.mo
